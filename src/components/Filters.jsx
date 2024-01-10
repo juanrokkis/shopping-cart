874d4e3e2ef0e6adf0ebd200b0c4413273e5db84
juanrokkis <xxjuanquesadaxx@gmail.com>
2024-01-10 16:05:00 -0500

    initial commit

---
+++ src/components/Filters.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import "./Filters.css";
+
+export function Filters({ onChange }) {
+  const [minPrice, setMinPrice] = useState(0);
+
+  const handleChangeMinPrice = (event) => {
+    setMinPrice(event.target.value);
+    onChange((prevState) => ({
+      ...prevState,
+      minPrice: event.target.value,
+    }));
+  };
+
+  const handleChangeCategory = (event) => {
+    onChange((prevState) => ({
+      ...prevState,
+      category: event.target.value,
+    }));
+  };
+
+  return (
+    <section className="filters">
+      <div>
+        <label htmlFor="price">Price</label>
+        <input
+          type="range"
+          id="price"
+          min="0"
+          max="1000"
+          onChange={handleChangeMinPrice}
+        />
+        <span>${minPrice}</span>
+      </div>
+
+      <div>
+        <label htmlFor="category">Category</label>
+        <select name="" id="category" onChange={handleChangeCategory}>
+          <option value="all">All</option>
+          <option value="laptops">Laptops</option>
+          <option value="smartphones">Smartphones</option>
+        </select>
+      </div>
+    </section>
+  );
+}
